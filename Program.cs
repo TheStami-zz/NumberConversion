78813d0a0db22b425c0efe8539db55fcbb4712f9
TheStami <kandziopl@interia.pl>
2021-02-11 10:08:51 +0100

    Init repository

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A15.Math.Convertion {
+    class NumberConvertion {
+        static void Main(string[] args) {
+            
+        }
+
+        public static int Convert(int baseSystem, int destinationSystem, int number) {
+            return 0;
+        }
+    }
+}
